d88dd011c30af044f62541d1a170a6563f5a918d
sukai182 <kai.su@dilu.com>
2022-01-29 10:57:29 +0800

    add manifests dex

--- manifests/dex/gencert.sh
+++ manifests/dex/gencert.sh
@@ -19,21 +19,24 @@ DNS.1 = dex.aiscope.io
 EOF
 
 openssl genrsa -out ssl/ca-key.pem 2048
-openssl req -x509 -new -nodes -key ssl/ca-key.pem -days 10 -out ssl/ca.pem -subj "/CN=kube-ca"
+openssl req -x509 -new -nodes -key ssl/ca-key.pem -days 10 -out ssl/ca.pem -subj "/CN=aiscope"
 
 openssl genrsa -out ssl/key.pem 2048
-openssl req -new -key ssl/key.pem -out ssl/csr.pem -subj "/CN=kube-ca" -config ssl/req.cnf
+openssl req -new -key ssl/key.pem -out ssl/csr.pem -subj "/CN=aiscope" -config ssl/req.cnf
 openssl x509 -req -in ssl/csr.pem -CA ssl/ca.pem -CAkey ssl/ca-key.pem -CAcreateserial -out ssl/cert.pem -days 10 -extensions v3_req -extfile ssl/req.cnf
 
 kubectl create ns dex
 kubectl -n dex create secret tls dex.aiscope.io.tls \
   --cert=ssl/cert.pem \
   --key=ssl/key.pem
+kubectl -n dex create secret generic dex.aiscope.io.ca --from-file=ssl/ca.pem
 
-CERT_NAME=dex.aiscope.io.tls
-HOST=dex.aiscope.io
-CERT_FILE=tls.cert
-KEY_FILE=tls.key
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${KEY_FILE} -out ${CERT_FILE} -subj "/CN=${HOST}/O=${HOST}"
-kubectl create ns dex
-kubectl -n dex create secret tls ${CERT_NAME} --key ${KEY_FILE} --cert ${CERT_FILE}
+#CERT_NAME=dex.aiscope.io.tls
+#HOST=dex.aiscope.io
+#CERT_FILE=tls.cert
+#KEY_FILE=tls.key
+#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${KEY_FILE} -out ${CERT_FILE} -subj "/CN=${HOST}/O=${HOST}"
+#kubectl create ns dex
+#kubectl -n dex create secret tls ${CERT_NAME} --key ${KEY_FILE} --cert ${CERT_FILE}
+
+#http://dex.aiscope.io:32000/auth?client_id=aiscope&redirect_uri=http://api.aiscope.io:9090/oauth/callback/dex&response_type=code&scope=openid+email+groups+profile+offline_access
